ce7063030c006a7d29dbf45a8f0ef3bfcece96de
Huy Nguyen <hnguyen11@atlassian.com>
2018-01-09 17:00:25 -0800

    Convert project to be Carthage compatible.
    * This mostly involved creating a framework project and making it
      shared. The dependencies are not private as this project depends on
      both EarlGrey and FBSnapshotTestCase.

---
+++ EarlGreySnapshots/EarlGreySnapshots.h
@@ -0,0 +1,19 @@
+//
+//  EarlGreySnapshots.h
+//  EarlGreySnapshots
+//
+//  Created by Huy Nguyen on 1/9/18.
+//  Copyright © 2018 Huy Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EarlGreySnapshots.
+FOUNDATION_EXPORT double EarlGreySnapshotsVersionNumber;
+
+//! Project version string for EarlGreySnapshots.
+FOUNDATION_EXPORT const unsigned char EarlGreySnapshotsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EarlGreySnapshots/PublicHeader.h>
+
+
